6bc161298a2b7cfbfd7df650875357aba3b907ac
Sharlikran <danielhmpdx16@gmail.com>
2020-06-04 00:22:31 -0700

    Version: 1.87

--- Init.lua
+++ Init.lua
@@ -1,4 +1,4 @@
-local libName, libVersion = "LibQuestInfo", 186
+local libName, libVersion = "LibQuestInfo", 187
 lib = {}
 internal = {}
 lib.quest_givers = {}
@@ -49,80 +49,84 @@ if LibQuestInfo_SavedVariables.giver_names == nil then LibQuestInfo_SavedVariabl
 -- note only the "lib.client_lang" will contain data be default
 
 lib.quest_data_index = {
-    QUEST_NAME      =    1, -- Number index of quest name i.e. 6404 for "The Dragonguard"
-    QUEST_GIVER     =    2, -- Arbitrary number pointing to an NPC Name 81004, "Abnur Tharn"
-    QUEST_TYPE      =    3, -- MAIN_STORY, DUNGEON
-    QUEST_REPEAT    =    4, -- QUEST_REPEAT_DAILY, QUEST_REPEAT_NOT_REPEATABLE = 0, QUEST_REPEAT_REPEATABLE
-    GAME_API        =    5, -- 100003 means unverified, 100030 means quest data collected from API 100030
-    QUEST_LINE      =    6,    -- QuestLine (10000 = not assigned/not verified. 10001 = not part of a quest line/verified)
-    QUEST_NUMBER    =    7,    -- Quest Number In QuestLine (10000 = not assigned/not verified)
-    QUEST_SERIES    =    8,    -- None = 0,    Cadwell's Almanac = 1,    Undaunted = 2, AD = 3, DC = 4, EP = 5.
+    quest_name      =    1, -- Number index of quest name i.e. 6404 for "The Dragonguard"
+    quest_giver     =    2, -- Depreciated, see quest_map_pin_index
+    quest_type      =    3, -- MAIN_STORY, DUNGEON
+    quest_repeat    =    4, -- quest_repeat_daily, quest_repeat_not_repeatable = 0, quest_repeat_repeatable
+    game_api        =    5, -- 100003 means unverified, 100030 means quest data collected from API 100030
+    quest_line      =    6,    -- QuestLine (10000 = not assigned/not verified. 10001 = not part of a quest line/verified)
+    quest_number    =    7,    -- Quest Number In QuestLine (10000 = not assigned/not verified)
+    quest_series    =    8,    -- None = 0,    Cadwell's Almanac = 1,    Undaunted = 2, AD = 3, DC = 4, EP = 5.
 }
 
 lib.quest_map_pin_index = {
-    X_LOCATION  =    1, -- GetMapPlayerPosition() << -10 = Undefined >>
-    Y_LOCATION  =    2, -- GetMapPlayerPosition() << -10 = Undefined >>
-    X_LIBGPS    =    3, -- LocalToGlobal(GetMapPlayerPosition()) << -10 = Undefined >>
-    Y_LIBGPS    =    4, -- LocalToGlobal(GetMapPlayerPosition()) << -10 = Undefined >>
-    QUEST_ID    =    5, -- Number index of quest name i.e. 6404 for "The Dragonguard"
+    local_x     =    1, -- GetMapPlayerPosition() << -10 = Undefined >>
+    local_y     =    2, -- GetMapPlayerPosition() << -10 = Undefined >>
+    global_x    =    3, -- LocalToGlobal(GetMapPlayerPosition()) << -10 = Undefined >>
+    global_y    =    4, -- LocalToGlobal(GetMapPlayerPosition()) << -10 = Undefined >>
+    quest_id    =    5, -- Number index of quest name i.e. 6404 for "The Dragonguard"
+    world_x     =    6, -- WorldX 3D Location
+    world_y     =    7, -- WorldY 3D Location
+    world_z     =    8, -- WorldZ 3D Location
+    quest_giver =    9, -- Arbitrary number pointing to an NPC Name 81004, "Abnur Tharn"
 }
 
 lib.quest_data_type = {
     -- ESO Values
-    QUEST_TYPE_NONE = 0,
-    QUEST_TYPE_GROUP = 1,
-    QUEST_TYPE_MAIN_STORY = 2,
-    QUEST_TYPE_GUILD = 3,
-    QUEST_TYPE_CRAFTING = 4,
-    QUEST_TYPE_DUNGEON = 5,
-    QUEST_TYPE_RAID = 6,
-    QUEST_TYPE_AVA = 7, -- None in table, in check
-    QUEST_TYPE_CLASS = 8, -- None in table
-    QUEST_TYPE_AVA_GROUP = 10, -- None in table, in check
-    QUEST_TYPE_AVA_GRAND = 11, -- None in table, in check
-    QUEST_TYPE_HOLIDAY_EVENT = 12, -- None in table
-    QUEST_TYPE_BATTLEGROUND = 13, -- None in table
+    quest_type_none = 0,
+    quest_type_group = 1,
+    quest_type_main_story = 2,
+    quest_type_guild = 3,
+    quest_type_crafting = 4,
+    quest_type_dungeon = 5,
+    quest_type_raid = 6,
+    quest_type_ava = 7, -- None in table, in check
+    quest_type_class = 8, -- None in table
+    quest_type_ava_group = 10, -- None in table, in check
+    quest_type_ava_grand = 11, -- None in table, in check
+    quest_type_holiday_event = 12, -- None in table
+    quest_type_battleground = 13, -- None in table
 
     -- LibQuestInfo Values
-    QUEST_TYPE_DAILY            = 14,
-    QUEST_TYPE_CADWELL          = 15,
-    QUEST_TYPE_AD               = 16,
-    QUEST_TYPE_DC               = 17,
-    QUEST_TYPE_EP               = 18,
-    QUEST_TYPE_UNDEFINED        = 19,
-    QUEST_TYPE_GUILD_MAGE       = 20,
-    QUEST_TYPE_GUILD_FIGHTER    = 21,
-    QUEST_TYPE_GUILD_PSIJIC     = 22,
-    QUEST_TYPE_GUILD_THIEF      = 23,
-    QUEST_TYPE_GUILD_DARK       = 24, -- Dark Brotherhood
-    QUEST_TYPE_UNDAUNTED        = 25,
+    quest_type_daily            = 14,
+    quest_type_cadwell          = 15,
+    quest_type_ad               = 16,
+    quest_type_dc               = 17,
+    quest_type_ep               = 18,
+    quest_type_undefined        = 19,
+    quest_type_guild_mage       = 20,
+    quest_type_guild_fighter    = 21,
+    quest_type_guild_psijic     = 22,
+    quest_type_guild_thief      = 23,
+    quest_type_guild_dark       = 24, -- Dark Brotherhood
+    quest_type_undaunted        = 25,
 }
 
 lib.quest_data_repeat = {
-    QUEST_REPEAT_DAILY = 2,
-    QUEST_REPEAT_NOT_REPEATABLE = 0,
-    QUEST_REPEAT_REPEATABLE = 1,
+    quest_repeat_daily = 2,
+    quest_repeat_not_repeatable = 0,
+    quest_repeat_repeatable = 1,
 }
 
 lib.dest_quest_data_index = {
-	QUEST_X =		1,
-	QUEST_Y =		2,
-	QUESTNPC =		3,
-	QUESTTYPE = 	4,
-	QUESTREPEAT =	5,
-	QUESTID =		6,
-	QUESTLINE =		7,	-- QuestLine (10000 = not assigned/not verified. 10001 = not part of a quest line/verified)
-	QUESTNUMBER =	8,	-- Quest Number In QuestLine (10000 = not assigned/not verified)
-	QUESTSERIES =	9,	-- None = 0,	Cadwell's Almanac = 1,	Undaunted = 2, AD = 3, DC = 4, EP = 5.
+	quest_x =		1,
+	quest_y =		2,
+	questnpc =		3,
+	questtype = 	4,
+	questrepeat =	5,
+	questid =		6,
+	questline =		7,	-- QuestLine (10000 = not assigned/not verified. 10001 = not part of a quest line/verified)
+	questnumber =	8,	-- Quest Number In QuestLine (10000 = not assigned/not verified)
+	questseries =	9,	-- None = 0,	Cadwell's Almanac = 1,	Undaunted = 2, AD = 3, DC = 4, EP = 5.
 }
 
 lib.dest_quest_series_index = {
-    QUEST_SERIES_NONE       = 0,
-    QUEST_SERIES_CADWELL    = 1,
-    QUEST_SERIES_UNDAUNTED  = 2,
-    QUEST_SERIES_AD         = 3,
-    QUEST_SERIES_DC         = 4,
-    QUEST_SERIES_EP         = 5,
+    quest_series_none       = 0,
+    quest_series_cadwell    = 1,
+    quest_series_undaunted  = 2,
+    quest_series_ad         = 3,
+    quest_series_dc         = 4,
+    quest_series_ep         = 5,
 }
 
 _G["LibQuestInfo"] = lib
