663466f2078f8a955d3975745848f6da408238e1
Sharlikran <danielhmpdx16@gmail.com>
2021-11-23 18:51:53 -0800

    Version: 2.33

--- Init.lua
+++ Init.lua
@@ -1,4 +1,4 @@
-local libName, libVersion   = "LibQuestData", 232
+local libName, libVersion   = "LibQuestData", 233
 local lib                   = {}
 local internal              = {}
 _G["LibQuestData"]          = lib
